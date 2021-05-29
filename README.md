54a58405a6ed477b91443a30ee8fe98d2bf1da21
Krishna Nanduri <krishna.21nv@gmail.com>
2021-05-29 10:23:02 +0530

    added lazy loading

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ Get Inspired
 
 ## Features
 
--
+- View All Quotes
+- Add Quote
+- View Quote Details
+- View Comments
+- Add Comment
 
 ## Technology
 
 - Frontend: React.js
+- Routing: React Router
 
 ## Website
+
+-
 
 ---
 
--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
+import { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 import HomePage from "./pages/HomePage";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
-import NotFound from "./pages/NotFound";
+
+// import AllQuotes from "./pages/AllQuotes";
+// import NewQuote from "./pages/NewQuote";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import NotFound from "./pages/NotFound";
+
+// Lazy Loading
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const NotFound = lazy(() => import("./pages/NewQuote"));
 
 const App = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact>
-          <HomePage />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/new" exact>
-          <NewQuote />
-        </Route>
-        <Route path="/quotes/:id">
-          <QuoteDetail />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            <HomePage />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/new" exact>
+            <NewQuote />
+          </Route>
+          <Route path="/quotes/:id">
+            <QuoteDetail />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
