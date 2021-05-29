9c22e11d9035c43d170b9ea6d898aafa51ea9e03
Krishna Nanduri <krishna.21nv@gmail.com>
2021-05-29 10:29:53 +0530

    deloyment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+
+// Not Supported by GitHub Pages
+// import { BrowserRouter } from "react-router-dom";
+
+// Suppoerted by GitHub Pages
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 
@@ -8,9 +13,11 @@ import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>,
   document.getElementById("root")
 );
